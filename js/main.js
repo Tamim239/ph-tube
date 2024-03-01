2e9aca6a95ae0fd63a5205cc20189cf68d47c7aa
“Tamim239” <mdibrahimislamtamim239@gmail.com>
2024-03-01 16:50:03 +0600

    added blog html and pubslish date time

--- js/main.js
+++ js/main.js
@@ -63,6 +63,7 @@ const categoryDetailsApi = async (id, shorted) => {
 }
 
 let isVerified = '';
+let isTime = '';
 const categoryDetails = (id) => {
     const cardContainer = document.getElementById("card-container")
     cardContainer.textContent = ''
@@ -71,14 +72,18 @@ const categoryDetails = (id) => {
         if (data.authors[0].verified) {
             isVerified = ` <img src="images/fi_10629607.svg" alt="logo">`
         }
+        if(data?.others?.posted_date){
+            isTime = convertTime(data?.others?.posted_date)   
+        }
+        console.log(convertTime(data?.others?.posted_date))
         const div = document.createElement("div")
         div.classList = "card card-compact shadow-xl py-2 h-80";
         div.innerHTML = `
-                    <figure><img src="${data.thumbnail}" /></figure>
+                    <figure class="h-1/2"><img src="${data.thumbnail}" /></figure>
                     <div class="">
                       <div class="flex gap-3 space-y-3">
                         <img src="${data?.authors[0]?.profile_picture}" alt="picture" class="relative size-10 rounded-full top-4">
-                        <p class="absolute top-28 right-4 bg-gray-800 text-white p-1 rounded-lg">3hrs 56 min ago</p>
+                        <p class="absolute top-28 right-4 bg-gray-800 text-white p-1 rounded-lg">${isTime} ago</p>
                          <div class="space-y-2">
                             <p class="font-bold">Building a Winning UX Strategy Using the Kano Model</p>
                          </div>
@@ -96,6 +101,25 @@ const categoryDetails = (id) => {
 
 }
 
+// convert hours minutes seconds
+function convertTime(milliSeconds){
+    const convert = parseFloat(milliSeconds)
+    let seconds = (convert / 1000).toFixed(0);
+    let minutes = Math.floor(seconds / 60);
+    let hours = "";
+    if(minutes > 59){
+        hours = Math.floor(minutes / 60);
+        hours = (hours >= 10) ? hours : "0" + hours;
+        minutes = minutes - (hours * 60);
+        minutes = (minutes >= 10) ? minutes : "0" + minutes;
+    }
+    seconds = Math.floor(seconds % 60);
+        seconds = (seconds >= 10) ? seconds : "0" + seconds;
+        if (hours != "") {
+            return hours + "hrs" + minutes + "min" + seconds + "sec";
+        }
+        return minutes + "min" + seconds + "sec";
+}
 
 categoriesAPi();
 categoryDetailsApi( 1000,shorted);
